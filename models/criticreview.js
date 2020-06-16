82d7d4c943ff2fa0018a9ed6a1c091dc384853e8
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-15 22:16:35 -0400

    Add models and db

---
+++ models/criticreview.js
@@ -0,0 +1,14 @@
+// var mongoose = require('mongoose');
+
+// const reviewSchema = new mongoose.Schema({
+//   name: String,
+//   email: String,
+//   cohort: String,
+//   avatar: String,
+//   facts: [factSchema],
+//   googleId: String,
+// }, {
+//   timestamps: true
+// });
+
+// module.exports = mongoose.model('Student', studentSchema);
