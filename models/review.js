cf04114759e3d91be6295b94aa45e4e9957ae3a0
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-18 11:54:54 -0400

    Some unecessary changes

--- models/review.js
+++ models/review.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
   {
+    name: String,
     content: String,
     rating: Number,
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     name: String,
     email: String,
+    avatar: String,
     googleId: String,
   },
   {
