041b3f09fda1d2a2bee77f3dcc4125a06feea42f
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-18 15:25:48 -0400

    Added deleteon function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,14 +3,19 @@ const PitchforkReview = require("../models/pitchforkreview");
 
 module.exports = {
   create,
+  delete: deleteOne,
 };
 
 function create(req, res) {
-  // when comment is created the pitchforkReview is set to the current album reviews id
   req.body.pitchforkReview = req.params.id;
-  // when comment is created the user is set to the user that made the POST request
   req.body.user = req.user;
   UserReview.create(req.body, function (err, userReview) {
     res.redirect("/show/" + req.params.id);
+  });
+}
+
+function deleteOne(req, res) {
+  UserReview.remove({ _id: req.params.commentId }, function () {
+    res.redirect("/show/" + req.params.reviewId);
   });
 }
